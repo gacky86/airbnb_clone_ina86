c08d06e88585d593794c1339d0371f6f5fe184dc
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2024-12-27 21:02:23 +0100

    create a search box in navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,28 @@ class PagesController < ApplicationController
 
   def home
     @houses = House.all
+    @requests = Request.where("status = ? OR status = ?", "PENDING", "ACCEPTED")
+    # puts params
+    # houseからrequestにアクセスして、pending/accepted のstatusを持つリクエストを持つhouse以外を抽出する
+    if params[:search][:start_date].present?
+
+      start_date_query = Date.parse(params[:search][:start_date])
+      end_date_query = Date.parse(params[:search][:end_date])
+
+      @houses.each do |house_a|
+        requests = house_a.requests
+        next if requests.empty?
+
+        requests.each do |request|
+          unless end_date_query < request.start_date || request.end_date < start_date_query
+            @houses = @houses.reject { |house| house == house_a }
+            break
+          end
+        end
+      end
+
+      @houses = @houses.where("country ILIKE ?", params[:search][:country])
+    end
   end
 
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,4 +2,4 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
-import "bootstrap"　//追加
+import "bootstrap"//追加
