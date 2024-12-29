b3ca555dcb205fb538edeaf9d9943c42f9999481
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2024-12-29 21:16:08 +0100

    refine bookmark code

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,8 @@
 class Request < ApplicationRecord
   belongs_to :user
   belongs_to :house
+
+  VALID_STATUS = ["PENDING", "ACCEPTED", "REJECTED"].freeze
+
+  validates :status, presence: true, inclusion: { in: VALID_STATUS }
 end
